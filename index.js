f23e3b608680bc4d572201d6848fb0befdce0deb
josephquested <josephquested@gmail.com>
2016-05-17 17:27:53 +1200

    add MVP

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // --- TABLE ---
-
 function generateBoard (size) {
   for (let i = 0; i < size; i++) {
     $('#table').append(generateRowElement(i))
@@ -19,10 +18,10 @@ function generateCellElement (rowNumber, cellNumber) {
 
 function returnCellByDirection (oldCell, direction) {
   let cells = {
-    1: $(oldCell).closest('tr').prev().children().eq($(oldCell).index()),
-    2: $(oldCell).next(),
-    3: $(oldCell).closest('tr').next().children().eq($(oldCell).index()),
-    4: $(oldCell).prev()
+    1: oldCell.closest('tr').prev().children().eq(oldCell.index()),
+    2: oldCell.next(),
+    3: oldCell.closest('tr').next().children().eq(oldCell.index()),
+    4: oldCell.prev()
   }
   return cells[direction]
 }
@@ -30,33 +29,52 @@ function returnCellByDirection (oldCell, direction) {
 function returnRandomCell () {
   let cells = $('td').toArray()
   let randomCell = $(cells[returnRandomInt(cells.length)])
-  while ($(randomCell).hasClass('player') || $(randomCell).hasClass('tail')) {
+  while (randomCell.hasClass('player') || randomCell.hasClass('tail')) {
     randomCell = $(cells[returnRandomInt(cells.length)])
   }
   return randomCell
 }
 
 // --- MOVEMENT ---
-
 var movementInterval
+var previousCell
 var playerSpeed = 300
 var direction = 3
 
 function handleInputs (e) {
+  var newDirection
+  if (e.which === 38) newDirection = 1
+  if (e.which === 39) newDirection = 2
+  if (e.which === 40) newDirection = 3
+  if (e.which === 37) newDirection = 4
   window.clearInterval(movementInterval)
-  if (e.which === 38) direction = 1
-  if (e.which === 39) direction = 2
-  if (e.which === 40) direction = 3
-  if (e.which === 37) direction = 4
-  handleMovement()
+  handleMovement(newDirection)
   startMovementInterval()
 }
 
-function handleMovement () {
+function directionValid (newDirection) {
+  if (tail.length > 0) {
+    if (direction === 1 && newDirection === 3) return false
+    if (direction === 2 && newDirection === 4) return false
+    if (direction === 3 && newDirection === 1) return false
+    if (direction === 4 && newDirection === 2) return false
+  }
+  return true
+}
+
+function handleMovement (newDirection) {
+  if (newDirection) {
+    if (directionValid(newDirection)) {
+      direction = newDirection
+    } else {
+      return
+    }
+  }
   var oldCell = $('.player')
   var newCell = returnCellByDirection(oldCell, direction)
   checkForCollisions(newCell)
   movePlayer(oldCell, newCell)
+  updateTail(oldCell)
 }
 
 function movePlayer (oldCell, newCell) {
@@ -71,13 +89,11 @@ function startMovementInterval () {
 }
 
 // --- PLAYER ---
-
 function spawnPlayer (playerCell) {
   playerCell.addClass('player')
 }
 
 // --- FOOD ---
-
 var foodSpawnFrequency = 2000
 
 function startFoodInterval () {
@@ -99,16 +115,31 @@ function spawnFood () {
 
 function consumeFood (foodCell) {
   foodCell.removeClass('food')
+  foodCell.addClass('tail')
+  tail.unshift(foodCell.attr('id'))
+}
+
+// --- TAIL ---
+var tail = []
+
+function updateTail (newCell) {
+  var previousCell
+  for (let i = 0; i < tail.length; i++) {
+    $(tail[i]).removeClass('tail')
+    $(newCell).addClass('tail')
+    previousCell = tail[i]
+    tail[i] = newCell
+    newCell = previousCell
+  }
 }
 
 // --- UTILS ---
-
 function returnRandomInt (max) {
   return Math.floor(Math.random() * (max - 0 + 1)) + 0
 }
 
 function checkForCollisions (newCell) {
-  if (!newCell.is('td')) gameOver()
+  if (!newCell.is('td') || newCell.hasClass('tail')) gameOver()
   if (newCell.hasClass('food')) consumeFood(newCell)
 }
 
@@ -118,7 +149,6 @@ function gameOver () {
 }
 
 // --- RUN ---
-
 $(function () {
   generateBoard(15)
   spawnPlayer($('#cell_0_7'))
